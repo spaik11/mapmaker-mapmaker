37ea29fbf42e026b4c75f5d0a0b7b0a597beaeb3
Sung Paik <sung.paik@codeimmersives.com>
2019-10-22 16:05:07 -0400

    add1ToLeft left to do

--- main.js
+++ main.js
@@ -1,22 +1,68 @@
-function doubleAll() {
+function doubleAll(arr) {
+  const newArr = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i] * 2);
+  }
+  return newArr;
 }
 
-function absoluteValues() {
+function absoluteValues(arr) {
+  const newArr = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] < 0) {
+      newArr.push(arr[i] * -1 || arr[i] === 0)
+    } else {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr;
 }
 
-function yelledGreetings() {
+function yelledGreetings(arr) {
+  const newArr = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    newArr.push(`${arr[i]}!`)
+  }
+  return newArr;
 }
 
-function changeToInitials() {
+function changeToInitials(arr) {
+  const initials = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    initials.push(arr[i][0] + arr[i][arr[i].indexOf(' ') + 1])
+  }
+  return initials;
 }
 
-function doubleOdd() {
+function doubleOdd(arr) {
+  const newArr = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 === 1 || arr[i] % 2 === -1) {
+      newArr.push(arr[i] * 2);
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 }
 
-function upperCaseFirstLetters() {
+function upperCaseFirstLetters(arr) {
+  const newArr = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i][0].toUpperCase() + arr[i].slice(1).toLowerCase());
+  }
+  return newArr;
 }
 
-function add1ToLeft() {
+function add1ToLeft(arr) {
+  const newArr = [];
+
 }
 
 
