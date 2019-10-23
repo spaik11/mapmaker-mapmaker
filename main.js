c241110527f81def441dc229b7a127881cba5542
Sung Paik <sung.paik@codeimmersives.com>
2019-10-23 11:12:23 -0400

    cleaned up code

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function absoluteValues(arr) {
 
   for(let i = 0; i < arr.length; i++) {
     if(arr[i] < 0) {
-      newArr.push(arr[i] * -1 || arr[i] === 0)
+      newArr.push(arr[i] * -1)
     } else {
       newArr.push(arr[i])
     }
